bee17629d4fff06c4449609cb1562a2ccd27eb86
Mario Aeby <spam@eMeidi.com>
2013-08-21 21:24:30 +0200

    Initialize

---
+++ euromillions.php
@@ -0,0 +1,78 @@
+<?php
+    $ranges = array(
+        'main' => array(
+            'min' => 1,
+            'max' => 50,
+            'count' => 5,
+            'numbersPerRow' => 5,
+        ),
+        'stars' => array(
+            'min' => 1,
+            'max' => 11,
+            'count' => 2,
+            'numbersPerRow' => 3,
+        )
+    );
+    
+    foreach($ranges as $key=>$attribs) {
+        $numbers = generateSet($attribs);
+        $layout = getLayout($numbers,$attribs);
+        
+        print strtoupper($key) . "\n" . str_repeat('=',strlen($key)) . "\n" . $layout . "\n";
+    }
+    
+    function generateSet($attribs) {
+        $min = $attribs['min'];
+        $max = $attribs['max'];
+        $count = $attribs['count'];
+        
+        $out = array();
+        $current = 0;
+        while($current < $count) {
+            $current++;
+            
+            $duplicate = true;
+            while($duplicate) {
+                $number = rand($min,$max);
+                
+                if(!isset($out[$number])) {
+                    $out[$number] = $number;
+                    break;
+                }
+            }
+        }
+        
+        return $out;
+    }
+    
+    function getLayout($numbers,$attribs) {
+        $min = $attribs['min'];
+        $max = $attribs['max'];
+        $numbersPerRow = $attribs['numbersPerRow'];
+        
+        $out = null;
+        for($i = $min; $i<=$max; $i++) {
+            if(isset($numbers[$i])) {
+                $spacer = '|';
+            }
+            else {
+                $spacer = ' ';
+            }
+            
+            if($i < 10) {
+                $fill = ' ';
+            }
+            else {
+                $fill = null;
+            }
+            
+            $out .= $spacer . $fill . $i . $spacer . ' ';
+            
+            if($i % $numbersPerRow == 0) {
+                $out .= "\n";
+            }
+        }
+        
+        return $out;
+    }
+?>
